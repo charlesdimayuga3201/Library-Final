acb08deb2fec55becc910c4ad7fce7f958bc3ebd
lelouch3201 <charleskeane3201@gmail.com>
2022-06-02 05:12:10 +0800

    bug in borrowed book

--- Library Final.py
+++ Library Final.py
@@ -282,56 +282,66 @@ def returnbook():
 
 def returnbooks():
     connectdb()
-    if (e2.get() != ""):
-        n_var = 'SELECT bookids FROM BookBorrow WHERE borrowkey=%s'
-        n_val = (e2.get(),)
-        cur.execute(n_var,n_val)
-        n_result = cur.fetchone()
-        
-        if(n_result != None):
-            print (e2.get())
-            check1 = "SELECT status FROM Book WHERE bookid=%s"
-            ret1 = (n_result[0],)
-            cur.execute(check1, ret1)
-            result = cur.fetchone()
-            if(result != None):    
-                if(result[0] == 'Not Available'):
-                    check = "SELECT * FROM Book WHERE bookid=%s"
-                    ret = (ret1)
-                    cur.execute(check, ret)
-                    result = cur.fetchone()
-                    if (result == None):
-                        messagebox.showinfo("Result", "Book not found!")
-                    else:
-                        print (e2.get())
-                        r_book = 'UPDATE Book SET status = %s Where bookid = %s'
-                        s_book = "Available"
-                        o_result = (s_book, n_result[0])
-                        cur.execute(r_book,o_result)
-                        con.commit()
-                        
-                        
-                        a='UPDATE BookBorrow SET return_borrow = %s Where  borrowkey = %s'
-                        i=datetime.datetime(int(com1y.get()),month.index(com1m.get())+1,int(com1d.get()))
-                        i=i.isoformat()
-                        stats_ret = i 
-                        val = (stats_ret,e2.get()) 
-                        
-                        cur.execute(a,val)
-                        con.commit()
-                    
-                        messagebox.showinfo("Success", "Book Returned!")
-                        closedb()
-                        win.destroy()
-                        stud1()
-
-                else: 
-                    messagebox.showinfo("Success", "You Already Returned!")
-            else:
-                messagebox.showinfo("Result", "Book not found!")
     
-        else:
-            messagebox.showinfo("Error", "Borrow Key Not Found!")
+    if (e2.get() != ""):
+        s_var = 'SELECT stdid FROM BookBorrow where borrowkey = %s'
+        s_val = (e2.get())
+        cur.execute(s_var, s_val)
+        s_result = cur.fetchone()
+        print (e1.get())
+        if(s_result[0] == e1.get()):
+            
+            n_var = 'SELECT bookids FROM BookBorrow WHERE borrowkey=%s'
+            n_val = (e2.get(),)
+            cur.execute(n_var,n_val)
+            n_result = cur.fetchone()
+
+            if(n_result != None):
+                print (e2.get())
+                check1 = "SELECT status FROM Book WHERE bookid=%s"
+                ret1 = (n_result[0],)
+                cur.execute(check1, ret1)
+                result = cur.fetchone()
+                if(result != None):    
+                    if(result[0] == 'Not Available'):
+                        check = "SELECT * FROM Book WHERE bookid=%s"
+                        ret = (ret1)
+                        cur.execute(check, ret)
+                        result = cur.fetchone()
+                        if (result == None):
+                            messagebox.showinfo("Result", "Book not found!")
+                        else:
+                            print (e2.get())
+                            r_book = 'UPDATE Book SET status = %s Where bookid = %s'
+                            s_book = "Available"
+                            o_result = (s_book, n_result[0])
+                            cur.execute(r_book,o_result)
+                            con.commit()
+                            
+                            
+                            a='UPDATE BookBorrow SET return_borrow = %s Where  borrowkey = %s'
+                            i=datetime.datetime(int(com1y.get()),month.index(com1m.get())+1,int(com1d.get()))
+                            i=i.isoformat()
+                            stats_ret = i 
+                            val = (stats_ret,e2.get()) 
+                            
+                            cur.execute(a,val)
+                            con.commit()
+                        
+                            messagebox.showinfo("Success", "Book Returned!")
+                            closedb()
+                            win.destroy()
+                            stud1()
+
+                    else: 
+                        messagebox.showinfo("Success", "You Already Returned!")
+                else:
+                    messagebox.showinfo("Result", "Book not found!")
+        
+            else:
+                messagebox.showinfo("Error", "Borrow Key Not Found!")
+        else:        
+            messagebox.showinfo("Error", "Invalid!")
     else:
         messagebox.showinfo("Erro", "No Input!")
     
@@ -975,14 +985,14 @@ def deletebookborroweds():
         cur.execute(bk1,s_book1)
         d_book1 =cur.fetchone()
         if(d_book1 == None):
-            messagebox.showinfo("Message", "Book not found!")
+            messagebox.showinfo("Message", "Record not found!")
         else:
             if e2.get()=='admin':
                 q='DELETE FROM BookBorrow WHERE borrowkey="%i"'
                 cur.execute(q%(int(e1.get())))
                 con.commit()
                 
-                messagebox.showinfo("Success", "Book Deleted!")
+                messagebox.showinfo("Success", "Record Deleted!")
                 closedb()
                 win.destroy()
                 admin1()
